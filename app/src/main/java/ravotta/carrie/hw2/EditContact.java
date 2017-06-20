61d7cd3f8d558fca9c5e76f4a0305d0bc289ab06
Carrie Ravotta <carrie.peary@gmail.com>
2017-06-19 22:14:10 -0400

    WIP

---
+++ app/src/main/java/ravotta/carrie/hw2/EditContact.java
@@ -0,0 +1,10 @@
+package ravotta.carrie.hw2;
+
+import android.app.Activity;
+
+/**
+ * Created by carrie on 6/19/17.
+ */
+
+public class EditContact extends Activity {
+}
---
+++ app/src/main/java/ravotta/carrie/hw2/Main.java
@@ -0,0 +1,19 @@
+package ravotta.carrie.hw2;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by carrie on 6/19/17.
+ */
+
+public class Main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.contact_list);
+    }
+}
