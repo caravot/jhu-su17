ef9d59be183c89be26921eeeee4dafa2296fa6b6
Carrie Ravotta <carrie.peary@gmail.com>
2017-07-30 20:53:42 -0400

    WIP

--- app/build/generated/source/apt/debug/ravotta/carrie/hw5/BR.java
+++
@@ -1,6 +0,0 @@
-package ravotta.carrie.hw5;
-
-public class BR {
-        public static final int _all = 0;
-        public static final int item = 1;
-}
--- app/build/generated/source/dataBinding/debug/android/databinding/layouts/DataBindingInfo.java
+++
@@ -1,6 +0,0 @@
-package android.databinding.layouts;
-
-import android.databinding.BindingBuildInfo;
-
-@BindingBuildInfo(buildId="a8b67f4b-a077-4b7c-aec1-7defe2cbae4f")
-public class DataBindingInfo {}
