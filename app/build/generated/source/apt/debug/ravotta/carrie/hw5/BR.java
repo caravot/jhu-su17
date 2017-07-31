01b13ded5ac9ed3112124c941a9b53b2914119ec
Carrie Ravotta <carrie.peary@gmail.com>
2017-07-30 20:52:05 -0400

    Removed old files

--- app/build/generated/source/apt/debug/ravotta/carrie/hw5/BR.java
+++
@@ -1,8 +0,0 @@
-package ravotta.carrie.hw5;
-
-public class BR {
-        public static final int _all = 0;
-        public static final int item = 1;
-        public static final int model = 2;
-        public static final int position = 3;
-}
--- app/build/generated/source/dataBinding/debug/android/databinding/layouts/DataBindingInfo.java
+++
@@ -1,6 +0,0 @@
-package android.databinding.layouts;
-
-import android.databinding.BindingBuildInfo;
-
-@BindingBuildInfo(buildId="12e33f26-6727-47a2-bac9-00749cdda62b")
-public class DataBindingInfo {}
